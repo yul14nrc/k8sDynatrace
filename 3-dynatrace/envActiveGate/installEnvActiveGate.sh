0ed0491980014d81820f1615c925f5aea7f82b75
Julian Ruiz <yulianruiz@outlook.com>
2020-07-31 06:41:49 -0700

    added DT AG installation o azure VM

--- 3-dynatrace/envActiveGate/installEnvActiveGate.sh
+++ 3-dynatrace/envActiveGate/installEnvActiveGate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export GCP=false
+export azure=false
 
 http_response=$(curl -o /dev/null -s -w "%{http_code}\n" http://metadata.google.internal)
 if [ $http_response == 200 ]; then
@@ -8,6 +9,15 @@ if [ $http_response == 200 ]; then
     export DT_ENVIRONMENT_ID=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/environment_id -H "Metadata-Flavor: Google")
     export DT_PAAS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/paas_token -H "Metadata-Flavor: Google")
     GCP=true
+fi
+
+if [ $GCP == false ]; then
+    http_response=$(curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/compute/azEnvironment?api-version=2018-10-01&format=text")
+    if [[ $http_response =~ "AzurePublicCloud" ]]; then
+        azure=true
+        export DT_TENANT_ID=$1
+        export DT_PAAS_TOKEN=$2
+    fi
 fi
 
 if [ -z "$DT_ENVIRONMENT_ID" ]; then
@@ -29,8 +39,15 @@ sudo curl -o Dynatrace-ActiveGate-Linux.sh -X GET "$DT_TENANT_URL/api/v1/deploym
 if [[ -f "Dynatrace-ActiveGate-Linux.sh" ]]; then
     echo "ActiveGate File is downloaded"
     echo "AG Install Starting..."
-    sudo nohup /bin/sh /Dynatrace-ActiveGate-Linux.sh &
-    echo "AG Install Complete"
+    if [ $GCP == true ]; then
+        sudo nohup /bin/sh /Dynatrace-ActiveGate-Linux.sh &
+        echo "AG Install Complete"
+    fi
+    if [ $azure == true ]; then
+        chmod +x ./Dynatrace-ActiveGate-Linux.sh
+        sudo /bin/sh ./Dynatrace-ActiveGate-Linux.sh
+        echo "AG Install Complete"
+    fi
 else
     echo "Downloading again..."
     sudo curl -o Dynatrace-ActiveGate-Linux.sh -X GET "$DT_TENANT_URL/api/v1/deployment/installer/gateway/unix/latest" -H "accept: application/octet-stream" -H "Authorization: Api-Token $DT_PAAS_TOKEN"
@@ -69,6 +86,15 @@ if [ $GCP == true ]; then
     echo "" >>/var/lib/dynatrace/gateway/config/custom.properties
     echo '[azure_monitoring]' >>/var/lib/dynatrace/gateway/config/custom.properties
     echo 'azure_monitoring_enabled=false' >>/var/lib/dynatrace/gateway/config/custom.properties
+fi
+
+if [ $azure == true ]; then
+    echo "" >>/var/lib/dynatrace/gateway/config/custom.properties
+    echo '[aws_monitoring]' >>/var/lib/dynatrace/gateway/config/custom.properties
+    echo 'aws_monitoring_enabled=false' >>/var/lib/dynatrace/gateway/config/custom.properties
+    echo "" >>/var/lib/dynatrace/gateway/config/custom.properties
+    echo '[azure_monitoring]' >>/var/lib/dynatrace/gateway/config/custom.properties
+    echo 'azure_monitoring_enabled=true' >>/var/lib/dynatrace/gateway/config/custom.properties
 fi
 
 sudo service dynatracegateway restart
